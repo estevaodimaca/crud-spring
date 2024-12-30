ac7349e99bbd716db184ee327f495bae8552d24b
Estevao Pascoal Joao Dimaca <estevaodimaca@gmail.com>
2024-12-30 06:29:40 +0200

    criação do projecto

---
+++ src/test/java/mz/com/dimasoft/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.com.dimasoft.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
